7a5a956580b0462998b5b7c7ade38a35a01b69bc
irlanvl <amandavianiva@gmail.com>
2022-04-18 04:59:00 +0700

    setting up svelte and first versia

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  
+</script>
+
+<main>
+  <h1>Arayra Image Uploader</h1>
+  <p>Yahaha</p>
+</main>
+
+<style lang="scss">
+  @use './sass/_variables.scss' as *;
+
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      max-width: none;
+    }
+
+    p {
+      max-width: none;
+    }
+  }
+</style>
